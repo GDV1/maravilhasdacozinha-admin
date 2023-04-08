14c665c5dcf1d3a74253ac6743fbc9a494e951e8
Gabriel Dias <gabrieldv.dv@hotmail.com>
2023-04-07 23:31:58 -0300

    New module receipt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { NewReceiptComponent } from './new-receipt/new-receipt/new-receipt.component';
 
 const routes: Routes = [
-  { path: '', component: MenuComponent }
+  { path: '', component: MenuComponent },
+  { path: 'new-receipt', component: NewReceiptComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-exports: [RouterModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/receipt/new-receipt.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewReceiptComponent } from './new-receipt/new-receipt.component';
+
+@NgModule({
+  declarations: [
+    NewReceiptComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReceiptModule { }
